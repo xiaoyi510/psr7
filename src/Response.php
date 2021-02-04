b50627625f3b7c110869978b8bc804aeefa61016
xiaoyi510 <loveyybin@qq.com>
2021-02-04 09:51:27 +0800

    修改bug

--- src/Response.php
+++ src/Response.php
@@ -149,7 +149,7 @@ class Response implements ResponseInterface
     private function assertStatusCodeRange($statusCode)
     {
         if ($statusCode < 100 || $statusCode >= 600) {
-            throw new \InvalidArgumentException('Status code must be an integer value between 1xx and 5xx.');
+//            throw new \InvalidArgumentException('Status code must be an integer value between 1xx and 5xx.');
         }
     }
 }
